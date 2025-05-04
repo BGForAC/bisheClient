f6c8c8ba3e819458d251c973141ea500fd8bea80
gql <2412266175@qq.com>
2025-05-04 10:21:00 +0800

    save

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,9 @@ public class GameManager : MonoBehaviour
     internal static LuaEnv luaEnv = new LuaEnv();
     public TextAsset luaGlobalScript;
     public TextAsset luaTerisScript;
+    public TextAsset luaETerisScript;
     public TextAsset luaGameManagerScript;
+    public TextAsset luaEGameManagerScript;
 
     private LuaTable scriptScopeTable;
 
@@ -21,9 +23,21 @@ public class GameManager : MonoBehaviour
     private Action luaRotate;
     private Action luaUpdate;
     private Action luaInit;
+    public static Action luaGenrateNewBlock;
+
+    public static Action luaEMoveLeft;
+    public static Action luaEMoveRight;
+    public static Action luaEDrop;
+    public static Action luaERotate;
+    public static Action luaEClearRow;
+    public static Action luaEGenerateNewBlock;
+    public static Action luaEOnLand;
+    private Action luaEInit;
+
 
     void OnEnable()
     {
+        WebSocketClient.Connect();
         scriptScopeTable = luaEnv.NewTable();
 
         using (LuaTable meta = luaEnv.NewTable())
@@ -40,7 +54,9 @@ public class GameManager : MonoBehaviour
 
         luaEnv.DoString(luaGlobalScript.text, luaGlobalScript.name, scriptScopeTable);
         luaEnv.DoString(luaTerisScript.text, luaTerisScript.name, scriptScopeTable);
+        luaEnv.DoString(luaETerisScript.text, luaETerisScript.name, scriptScopeTable);
         luaEnv.DoString(luaGameManagerScript.text, luaGameManagerScript.name, scriptScopeTable);
+        luaEnv.DoString(luaEGameManagerScript.text, luaEGameManagerScript.name, scriptScopeTable);
 
         scriptScopeTable.Get("moveLeft", out luaMoveLeft);
         scriptScopeTable.Get("moveRight", out luaMoveRight);
@@ -48,9 +64,21 @@ public class GameManager : MonoBehaviour
         scriptScopeTable.Get("cancelDrop", out luaCancelDrop);
         scriptScopeTable.Get("rotate", out luaRotate);
         scriptScopeTable.Get("update", out luaUpdate);
+        scriptScopeTable.Get("generateNewBlock", out luaGenrateNewBlock);
+
+        scriptScopeTable.Get("eMoveLeft", out luaEMoveLeft);
+        scriptScopeTable.Get("eMoveRight", out luaEMoveRight);
+        scriptScopeTable.Get("eDrop", out luaEDrop);
+        scriptScopeTable.Get("eRotate", out luaERotate);
+        scriptScopeTable.Get("eClearRow", out luaEClearRow);
+        scriptScopeTable.Get("eGenerateNewBlock", out luaEGenerateNewBlock);
+        scriptScopeTable.Get("eOnLand", out luaEOnLand);
 
         scriptScopeTable.Get("init", out luaInit);
+        scriptScopeTable.Get("eInit", out luaEInit);
+
         luaInit?.Invoke();
+        luaEInit?.Invoke();
     }
 
     void MoveLeft()
@@ -80,6 +108,47 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
+        MainThreadDispatcher.Update();
         luaUpdate?.Invoke(); 
+    }
+
+    public static void GenerateNewBlock()
+    {
+        luaGenrateNewBlock?.Invoke();
+    }
+
+    internal static void EMoveLeft()
+    {
+        luaEMoveLeft?.Invoke();
+    }
+
+    internal static void EMoveRight()
+    {
+        luaEMoveRight?.Invoke();
+    }
+
+    internal static void EDrop()
+    {
+        luaEDrop?.Invoke();
+    }
+
+    internal static void ERotate()
+    {
+        luaERotate?.Invoke();
+    }
+
+    internal static void EClearRow()
+    {
+        luaEClearRow?.Invoke();
+    }
+
+    internal static void EGenerateNewBlock()
+    {
+        luaEGenerateNewBlock?.Invoke();
+    }
+
+    internal static void EOnLand()
+    {
+        luaEOnLand?.Invoke();
     }
 }
--- Assets/Scripts/WebSocketClient.cs
+++ Assets/Scripts/WebSocketClient.cs
@@ -6,19 +6,69 @@ using XLua;
 public class WebSocketClient : MonoBehaviour
 {
     private static WebSocket ws;
+    public static int blockNumber = -1;
+    public static int eBlockNumber = -1;
+    public static int eRowNumber = -1;
 
-    void Start()
+    public static void Connect()
     {
         // Connect to the WebSocket server
         ws = new WebSocket("ws://localhost:8080/ws");
 
         ws.OnMessage += (sender, e) =>
         {
-            Debug.Log("Message from server: " + e.Data);
+            MainThreadDispatcher.Enqueue(() =>
+            {
+                switch (e.Data)
+                {
+                    case string message when message.StartsWith("teris:"):
+                        blockNumber = int.Parse(message.Substring(6));
+                        GameManager.GenerateNewBlock();
+                        break;
+                    case string message when message.StartsWith("e "):
+                        reactTo(message.Substring(2));
+                        break;
+                    default:
+                        Debug.Log("Message from server: " + e.Data);
+                        break;
+                }
+
+                void reactTo(string message)
+                {
+                    switch (message)
+                    {
+                        case "moveLeft":
+                            GameManager.EMoveLeft();
+                            break;
+                        case "moveRight":
+                            GameManager.EMoveRight();
+                            break;
+                        case "drop":
+                            GameManager.EDrop();
+                            break;
+                        case "rotate":
+                            GameManager.ERotate();
+                            break;
+                        case "onLand":
+                            GameManager.EOnLand();
+                            break;    
+                        case string msg1 when msg1.StartsWith("clearRow:"):
+                            eRowNumber = int.Parse(msg1.Substring(9));
+                            GameManager.EClearRow();
+                            break;
+                        case string msg2 when msg2.StartsWith("teris:"):
+                            eBlockNumber = int.Parse(msg2.Substring(6));
+                            GameManager.EGenerateNewBlock();
+                            break;
+                        default:
+                            Debug.Log("Unknown message: " + message);
+                            break;
+                    }
+                }
+            });
         };
 
         ws.Connect();
-        ws.Send("Hello from Unity!");
     }
 
     public static void Send(string message)
